b0ddd3be4947e737aaab0bc7ccb3819b05804d5a
vit-ayuk <53066943+vit-ayuk@users.noreply.github.com>
2025-12-16 10:46:26 +0000

    Add files via upload

---
+++ interfaces/FrequencyDecisionEngine.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import core.DTNHost;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author jarkom
+ */
+public interface FrequencyDecisionEngine {
+    public Map<DTNHost, List<Duration>>  getFrequency();
+}
